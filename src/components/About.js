f7efd4d608dc25df905638684bdbb407c58d79df
parmod <parmodmagotra6391@gmail.com>
2023-09-12 18:31:23 +0530

    buld the instamart page

--- src/components/About.js
+++ src/components/About.js
@@ -4,33 +4,34 @@ import ProfileFunctional from "./Profile";
 import Profile from "./ProfileClass";
 import UserContext from "../utils/UserContext";
 class About extends Component {
+  constructor(props) {
+    super(props);
+  }
 
-    constructor(props) {
-        super(props);
+  componentDidMount() {
+    // api call
+  }
 
-    }
-
-    componentDidMount() {
-        // api call
-
-    }
-
-
-    render() {
-        return (
-            <div>
-                <h1>about us page</h1>
-                <p>finding the path</p>
-
+  render() {
+    return (
+      <div className="font-poppins">
+        <h1>about us page</h1>
+        <div className="w-full bg-[#bdecf1]">
+          <h1 className="text-4xl">
+            Our mission is to elevate the quality of life for the urban consumer
+            with unparalleled convenience. Convenience is what makes us tick.
+            It's what makes us get out of bed and say, "Let's do this."
+          </h1>
+        </div>
+        {/* 
                 <UserContext.Consumer>
                     {({ user }) => <h3>{user.email}</h3>}
                 </UserContext.Consumer>
 
-                <Profile name={"ningaClass"} />
-            </div>
-        )
-    }
+                <Profile name={"ningaClass"} /> */}
+      </div>
+    );
+  }
 }
-
 
 export default About;
--- src/components/Instamart.js
+++ src/components/Instamart.js
@@ -1,46 +1,85 @@
 import { useState } from "react";
 
-
-
 const Section = ({ title, description, isVisible, SetIsVisible }) => {
-    return (
-        <div className="border border-black p-3">
-            <h3 className="font-bold text-xl">{title}</h3>
-            {!isVisible ? <button className="cursor-pointer py-2 m-3 bg-lime-500" onClick={() => SetIsVisible(true)}>Show</button>
-                : <button className="cursor-pointer py-2 bg-lime-500" onClick={() => SetIsVisible(false)}>Hide</button>
-            }
-            {isVisible && <p>{description}</p>}
-        </div>
-    );
+  return (
+    <div className="border border-yellow-500 p-3">
+      <h3 className="font-bold text-xl">{title}</h3>
+      {!isVisible ? (
+        <button
+          className="cursor-pointer py-2 m-3 bg-lime-500"
+          onClick={() => SetIsVisible(true)}
+        >
+          Show
+        </button>
+      ) : (
+        <button
+          className="cursor-pointer py-2 bg-lime-500"
+          onClick={() => SetIsVisible(false)}
+        >
+          Hide
+        </button>
+      )}
+      {isVisible && <p>{description}</p>}
+    </div>
+  );
 };
-
 
 const Instamart = () => {
-    const [visibleSection, setVisibleSection] = useState("about");
-    return (
-        <div>
-            <h1 className="text-3xl font-bold p-4">Instamart</h1>
-            <Section title={"About Instamart"}
-                isVisible={visibleSection === "about"}
-                SetIsVisible={() => setVisibleSection("about")}
-
-                description={"this is about section of ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker includin instamart"} />
-
-            <Section title={"Team Instamart"}
-                isVisible={visibleSection === "team"}
-                SetIsVisible={() => setVisibleSection("team")}
-
-                description={"this is team section of instamart.Our team has 50 members ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker includin"} />
-
-            <Section title={"Career Instamart"}
-                isVisible={visibleSection === "career"}
-                SetIsVisible={() => setVisibleSection("career")}
-
-                description={"this is career section of instamart ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker includin"} />
-
-
+  const [visibleSection, setVisibleSection] = useState("about");
+  return (
+    <div>
+      <h1 className="text-3xl font-bold p-4 w-full bg-yellow-500">Instamart</h1>
+      {/* INSTAMART HERO SECTION */}
+      <div className="w-10/12 mx-auto font-poppins flex justify-between">
+        <div className="flex flex-col justify-center gap-20">
+          <h1 className="text-4xl font-bold ">
+            <span className="text-yellow-600 ">Convenience grocery,</span>
+            powered by Roti Time
+          </h1>
+          <ul className="flex flex-col gap-20">
+            <li className="text-2xl font-bold">
+              Groceries delivered in minutes , just as fast as we deliver food
+            </li>
+            <li className="text-2xl font-bold">
+              Open 6am to late night everyday, from your morning coffee needs to
+              snacks for your late night binge.
+            </li>
+          </ul>
         </div>
-    );
+
+        <div className="w-96 h-52 border border-green-500">
+          instamart logo.png
+        </div>
+      </div>
+
+      <Section
+        title={"About Instamart"}
+        isVisible={visibleSection === "about"}
+        SetIsVisible={() => setVisibleSection("about")}
+        description={
+          "this is about section of ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker includin instamart"
+        }
+      />
+
+      <Section
+        title={"Team Instamart"}
+        isVisible={visibleSection === "team"}
+        SetIsVisible={() => setVisibleSection("team")}
+        description={
+          "this is team section of instamart.Our team has 50 members ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker includin"
+        }
+      />
+
+      <Section
+        title={"Career Instamart"}
+        isVisible={visibleSection === "career"}
+        SetIsVisible={() => setVisibleSection("career")}
+        description={
+          "this is career section of instamart ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker includin"
+        }
+      />
+    </div>
+  );
 };
 
-export default Instamart
+export default Instamart;
